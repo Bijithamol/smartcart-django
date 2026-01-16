681a642459d7f9538c2521983d99ed8da0b0859d
Bijithamol <bijithapalathani@gmail.com>
2026-01-16 12:44:20 +0530

    added cartapp and updated smartcart features

--- smartCart/store/models.py
+++ smartCart/store/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from category.models import Category
+
 from django.urls import reverse
 
 class Product(models.Model):
@@ -10,7 +10,7 @@ class Product(models.Model):
     image           = models.ImageField(upload_to='photos/products')
     stock           = models.IntegerField()
     is_available    = models.BooleanField(default=True)
-    category        = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category        = models.ForeignKey('category.Category', on_delete=models.CASCADE)
     created_date    = models.DateTimeField(auto_now_add=True)
     modified_date   = models.DateTimeField(auto_now=True)
 

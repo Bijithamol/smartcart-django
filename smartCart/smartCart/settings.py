e57c99a14c82cd670ff03dcf65fbc2b2d33e71a8
Bijithamol <bijithapalathani@gmail.com>
2026-02-25 15:37:36 +0530

    added account details

--- smartCart/smartCart/settings.py
+++ smartCart/smartCart/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import AUTH_USER_MODEL
+from django.conf.global_settings import AUTH_USER_MODEL, EMAIL_HOST_PASSWORD, EMAIL_USE_TLS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,6 +133,22 @@ STATICFILES_DIRS = ['smartCart/static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
+
+
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+#smtp configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT =587
+EMAIL_HOST_USER ='bijithamolwork95@gmail.com'
+EMAIL_HOST_PASSWORD ='chvrufvowabjyrjp'
+EMAIL_USE_TLS = True
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- smartCart/smartCart/views.py
+++ smartCart/smartCart/views.py
@@ -5,3 +5,4 @@ def home(request):
     products=Product.objects.all().filter(is_available=True)
     context = {'products':products,}
     return render(request, 'home.html',context)
+
